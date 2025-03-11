1d6fd35a866dea7d23d4620de6637d3c49542953
Jeremy Gillick <j_gillick@yahoo.com>
2025-03-11 01:43:49 -0700

    Set package and cleanup

---
+++ src/pycybergear/__init__.py
@@ -0,0 +1,17 @@
+from pycybergear.motor_controller import CyberGearMotor
+from pycybergear.parameters import ParameterName, parameter_names
+from pycybergear.state import StateName, state_names
+from pycybergear.constants import (
+    RunMode,
+    KD_MAX,
+    KD_MIN,
+    KP_MAX,
+    KP_MIN,
+    P_MAX,
+    P_MIN,
+    T_MAX,
+    T_MIN,
+    V_MAX,
+    V_MIN,
+)
+from pycybergear.can import CyberMotorMessage
